60851d12d820c9256c0790353ef71a30038e4426
DEEPAK_KUMAR <154451485+Deepakkumarsvg@users.noreply.github.com>
2025-02-17 22:15:48 +0530

    Image add theme change (#2)
    * image add and theme change
    
    * image add and theme change

--- src/app/transmission/transmission.service.ts
+++ src/app/transmission/transmission.service.ts
@@ -6,162 +6,110 @@ import { ITransmissionResponse } from '../interfaces/transmission-responses';
 import { JWTService } from '../services/jwt.service';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class TransmissionService {
-
   constructor(
     private _http: HttpClient,
     private _httpErrorHandler: HttpErrorHandlerService,
-    private _jwt: JWTService,
+    private _jwt: JWTService
   ) {}
 
   public executeGetRequest<T extends ITransmissionResponse>(
-		url: string,
-		params?: { [key: string]: any },
-		requiresAuthentication: boolean = true
-	) {
-		return this._http.get<T>(url, {
-			headers: this._jwt.injectToken(requiresAuthentication),
-			params
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		);
-	}
+    url: string,
+    params?: { [key: string]: any },
+    requiresAuthentication: boolean = true
+  ) {
+    return this._http
+      .get<T>(url, {
+        headers: this._jwt.injectToken(requiresAuthentication),
+        params,
+      })
+      .pipe(catchError((error) => this._httpErrorHandler.intercept(error)));
+  }
 
   public async executeGetRequestPromise<T extends ITransmissionResponse>(
-		url: string,
-		params?: { [key: string]: any },
-		requiresAuthentication: boolean = true
-	) {
-
-    // this._loaderService.start()
-		const result: any =  await firstValueFrom(this._http.get<T>(url, {
-			headers: this._jwt.injectToken(requiresAuthentication),
-			params
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		));
-    // this._loaderService.stop();
-
+    url: string,
+    params?: { [key: string]: any },
+    requiresAuthentication: boolean = true
+  ) {
+    const result: any = await firstValueFrom(
+      this._http
+        .get<T>(url, {
+          headers: this._jwt.injectToken(requiresAuthentication),
+          params,
+        })
+        .pipe(catchError((error) => this._httpErrorHandler.intercept(error)))
+    );
     return result;
-	}
-
-// public async executeGetRequestPromise<T extends ITransmissionResponse>(
-//     url: string,
-//     params?: { [key: string]: any },
-//     requiresAuthentication: boolean = true
-// ) {
-//     const headers = requiresAuthentication ? this._jwt.injectToken(requiresAuthentication) : {};
-
-//     const result: any = await firstValueFrom(
-//         this._http.get<T>(url, {
-//             headers,
-//             params
-//         }).pipe(
-//             catchError((error) => this._httpErrorHandler.intercept(error))
-//         )
-//     );
-
-//     return result;
-// }
-
-// public async executeGetRequestPromise<T extends ITransmissionResponse>(
-//     url: string,
-//     params?: { [key: string]: any },
-//     requiresAuthentication: boolean = true
-// ) {
-//     let headers = {};
-
-//     if (requiresAuthentication) {
-//         const authHeader = this._jwt.injectToken(requiresAuthentication);
-//         if (authHeader && typeof authHeader === 'object') {
-//             headers = authHeader;  // Ensures that headers are only set when valid
-//         }
-//     }
-
-//     const result: any = await firstValueFrom(
-//         this._http.get<T>(url, {
-//             headers,
-//             params
-//         }).pipe(
-//             catchError((error) => this._httpErrorHandler.intercept(error))
-//         )
-//     );
-
-//     return result;
-// }
-
-
-
+  }
 
   public executePostRequest<T>(
-		url: string,
-		payload: any,
-		requiresAuthentication: boolean = true
-	) {
-		return this._http.post<ITransmissionResponse>(url, payload, {
-			headers: this._jwt.injectToken(requiresAuthentication)
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		);
-	}
+    url: string,
+    payload: any,
+    requiresAuthentication: boolean = true
+  ) {
+    return this._http
+      .post<ITransmissionResponse>(url, payload, {
+        headers: this._jwt.injectToken(requiresAuthentication),
+      })
+      .pipe(catchError((error) => this._httpErrorHandler.intercept(error)));
+  }
 
   public async executePostRequestPromise<T>(
-		url: string,
-		payload: any,
-		requiresAuthentication: boolean = true
-	) {
-
-    // this._loaderService.start();
-		const result: any =  await firstValueFrom(this._http.post<ITransmissionResponse>(url, payload, {
-			headers: this._jwt.injectToken(requiresAuthentication)
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		));
-    // this._loaderService.stop();
+    url: string,
+    payload: any,
+    requiresAuthentication: boolean = true
+  ) {
+    const result: any = await firstValueFrom(
+      this._http
+        .post<ITransmissionResponse>(url, payload, {
+          headers: this._jwt.injectToken(requiresAuthentication),
+        })
+        .pipe(catchError((error) => this._httpErrorHandler.intercept(error)))
+    );
 
     return result;
-	}
+  }
 
   public executePatchRequest<T>(
-		url: string,
-		payload: any,
-		requiresAuthentication: boolean = true
-	) {
-		return this._http.patch<ITransmissionResponse>(url, payload, {
-			headers: this._jwt.injectToken(requiresAuthentication)
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		);
-	}
+    url: string,
+    payload: any,
+    requiresAuthentication: boolean = true
+  ) {
+    return this._http
+      .patch<ITransmissionResponse>(url, payload, {
+        headers: this._jwt.injectToken(requiresAuthentication),
+      })
+      .pipe(catchError((error) => this._httpErrorHandler.intercept(error)));
+  }
 
   public async executePatchRequestPromise<T>(
-		url: string,
-		payload: any,
-		requiresAuthentication: boolean = true
-	) {
-    // this._loaderService.start();
-		const result: any =  await firstValueFrom(this._http.patch<ITransmissionResponse>(url, payload, {
-			headers: this._jwt.injectToken(requiresAuthentication)
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		));
-    // this._loaderService.stop();
+    url: string,
+    payload: any,
+    requiresAuthentication: boolean = true
+  ) {
+    const result: any = await firstValueFrom(
+      this._http
+        .patch<ITransmissionResponse>(url, payload, {
+          headers: this._jwt.injectToken(requiresAuthentication),
+        })
+        .pipe(catchError((error) => this._httpErrorHandler.intercept(error)))
+    );
 
     return result;
-	}
+  }
 
   public executeDeleteRequest<T>(
-		url: string,
-		params?: { [key: string]: any },
-		requiresAuthentication: boolean = true
-	) {
-		return this._http.delete<ITransmissionResponse>(url, {
-			headers: this._jwt.injectToken(requiresAuthentication),
-			params
-		}).pipe(
-			catchError((error) => this._httpErrorHandler.intercept(error))
-		);
-	}
+    url: string,
+    params?: { [key: string]: any },
+    requiresAuthentication: boolean = true
+  ) {
+    return this._http
+      .delete<ITransmissionResponse>(url, {
+        headers: this._jwt.injectToken(requiresAuthentication),
+        params,
+      })
+      .pipe(catchError((error) => this._httpErrorHandler.intercept(error)));
+  }
 }
