b94e45bb7fcb1abc12142a1d1eaa19de1ca5ec0c
Deepak kumar <deepakkumarsvg279@gmail.com>
2025-02-27 21:05:12 +0530

    homePageui

--- src/app/Component/urological/urological.component.ts
+++ src/app/Component/urological/urological.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
+import { SearchShowComponent } from '../search-show/search-show.component';
 
 @Component({
   selector: 'app-urological',
-  imports: [],
+  imports: [SearchShowComponent],
   templateUrl: './urological.component.html',
-  styleUrl: './urological.component.css'
+  styleUrl: './urological.component.css',
 })
-export class UrologicalComponent {
-
-}
+export class UrologicalComponent {}
