29bdfeddc35a1e0845e990e8407b9104aa21b801
Sushant Kumar <idsushant2793@gmail.com>
2025-02-27 21:23:04 +0530

    real time activity update

--- src/app/Component/welcome-add/welcome-add.component.ts
+++ src/app/Component/welcome-add/welcome-add.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { TransmissionService } from '../../transmission/transmission.service';
+import { RequestMapperService } from '../../services/request-mapper.service';
+import { interval, Subscription } from 'rxjs';
+import { switchMap } from 'rxjs/operators';
 
 @Component({
   selector: 'app-welcome-add',
@@ -7,5 +11,47 @@ import { Component } from '@angular/core';
   styleUrl: './welcome-add.component.css'
 })
 export class WelcomeAddComponent {
+  private refreshSubscription: Subscription = new Subscription();
+  public activeActivities: any[] = [];
+  
+  constructor(private transmit: TransmissionService) {}
 
+  async ngOnInit(): Promise<void> {
+    console.log("hi")
+    await this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`)
+    .then((res: any) => {
+      console.log("res",res);
+    })
+    await this.fetchActiveActivity();
+    
+    // 
+    this.refreshSubscription = interval(120000).pipe(
+      switchMap(() => this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`))
+    ).subscribe({
+      next: (data) => {
+        this.activeActivities = data;
+        console.log("Updated Active Activities:", this.activeActivities);
+      },
+      error: (error) => {
+        console.error("Error fetching active activities:", error);
+      },
+      complete: () => {
+        console.log("Active activities update completed.");
+      }
+    })
+  }
+
+  async fetchActiveActivity(): Promise<void> {
+    await this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`)
+    .then((res: any) => {
+        if(res.data) {
+          this.activeActivities = res.data;
+          console.log("active activity: ",this.activeActivities);
+        }
+      },
+      (error: any) => {
+        console.log("Error Fetching active activities: ",error);
+      }
+    )
+  }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,23 +3,33 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 import { RequestMapperService } from './services/request-mapper.service';
 import { TransmissionService } from './transmission/transmission.service';
 // import { WelcomeAddComponent } from './Component/welcome-add/welcome-add.component';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, RouterLink],
+  imports: [RouterOutlet, RouterLink, ReactiveFormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })  
 export class AppComponent {
   title = 'tet';
-
+  
+  public searchForm: FormGroup;
+  
   constructor(
-    private _transmit: TransmissionService
-  ) {}
-
+    private transmit: TransmissionService,
+    private fb: FormBuilder
+  ) {
+    this.searchForm = this.fb.group({
+      searchQuery: ['']
+    });
+  }
+  
   public async search(): Promise<void> {
-    console.log("clicked me");
-    await this._transmit.executeGetRequestPromise(`${RequestMapperService.API_CALL_CHECK}`)
+    console.log("Search Query:", );
+    const searchQuery = this.searchForm.get('searchQuery')?.value;
+    await this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}/${searchQuery}`)
     .then((res: any) => {
       console.log(res);
     })
