41f695d43b137b9795ab6675359d5f1e4dfce19a
isushant16 <90548151+isushant16@users.noreply.github.com>
2025-02-28 01:37:02 +0530

    real time activity display (#7)

--- src/app/Component/welcome-add/welcome-add.component.ts
+++ src/app/Component/welcome-add/welcome-add.component.ts
@@ -1,43 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { TransmissionService } from '../../transmission/transmission.service';
 import { RequestMapperService } from '../../services/request-mapper.service';
-import { interval, Subscription } from 'rxjs';
-import { switchMap } from 'rxjs/operators';
+import { interval, Subscription, from, BehaviorSubject  } from 'rxjs';
+import { startWith, switchMap, takeUntil } from 'rxjs/operators';
+import { DatePipe } from '@angular/common';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-welcome-add',
-  imports: [],
+  imports: [
+    CommonModule,
+    DatePipe
+  ],
   templateUrl: './welcome-add.component.html',
   styleUrl: './welcome-add.component.css'
 })
-export class WelcomeAddComponent {
-  private refreshSubscription: Subscription = new Subscription();
-  public activeActivities: any[] = [];
+export class WelcomeAddComponent implements OnInit, OnDestroy {
+  // private refreshSubscription: Subscription = new Subscription();
+  // public activeActivities: any[] = [];
+  activeActivities$ = new BehaviorSubject<any[]>([]); // Holds latest data
+  private subscription: any;
   
   constructor(private transmit: TransmissionService) {}
 
   async ngOnInit(): Promise<void> {
-    console.log("hi")
-    await this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`)
-    .then((res: any) => {
-      console.log("res",res);
-    })
-    await this.fetchActiveActivity();
-    
+    // await this.fetchActiveActivity();
     // 
-    this.refreshSubscription = interval(120000).pipe(
-      switchMap(() => this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`))
-    ).subscribe({
-      next: (data) => {
-        this.activeActivities = data;
-        console.log("Updated Active Activities:", this.activeActivities);
-      },
-      error: (error) => {
-        console.error("Error fetching active activities:", error);
-      },
-      complete: () => {
-        console.log("Active activities update completed.");
-      }
+    this.subscription = interval(10000).pipe(
+      startWith(0),
+      switchMap(() => from(this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`)))
+    ).subscribe((res: any) => {
+      console.log("res.data after 10 secs: ", res.data)
+      this.activeActivities$.next(Array.isArray(res.data) ? res.data  : []);
     })
   }
 
@@ -45,13 +39,24 @@ export class WelcomeAddComponent {
     await this.transmit.executeGetRequestPromise(`${RequestMapperService.FETCH_ACTIVITY_LIST}`)
     .then((res: any) => {
         if(res.data) {
-          this.activeActivities = res.data;
-          console.log("active activity: ",this.activeActivities);
+          this.activeActivities$ = res.data;
         }
       },
       (error: any) => {
         console.log("Error Fetching active activities: ",error);
       }
     )
+  }
+
+  formatDate(epoch: number): string {
+    const date = new Date(epoch);
+    return date.toLocaleString('en-US', { weekday: 'long', hour: '2-digit', minute: '2-digit', hour12: false });
+  }
+
+  ngOnDestroy(): void {
+    if(this.subscription) {
+      this.subscription.unsubscribe();
+      console.log('Unsubscribed from refresh interval');
+    }
   }
 }
