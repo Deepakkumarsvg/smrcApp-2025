9a8f9a02246241f8c101e3a3c298fdb9bbdf8892
DEEPAK_KUMAR <154451485+Deepakkumarsvg@users.noreply.github.com>
2025-02-27 21:10:09 +0530

    homePageui (#5)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { RequestMapperService } from './services/request-mapper.service';
 import { TransmissionService } from './transmission/transmission.service';
+import { Route } from '@angular/router';
 // import { WelcomeAddComponent } from './Component/welcome-add/welcome-add.component';
 
 @Component({
@@ -9,19 +10,33 @@ import { TransmissionService } from './transmission/transmission.service';
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
-})  
+})
 export class AppComponent {
   title = 'tet';
+  redirectToExternal(url: string) {
+    window.location.href = url;
+  }
+  isDrawerOpen = false;
 
-  constructor(
-    private _transmit: TransmissionService
-  ) {}
+  toggleDrawer(): void {
+    this.isDrawerOpen = !this.isDrawerOpen;
+  }
+
+  activeIndex = 0;
+
+  // Called when a nav item is clicked
+  setActive(index: number): void {
+    this.activeIndex = index;
+  }
+
+  constructor(private _transmit: TransmissionService) {}
 
   public async search(): Promise<void> {
-    console.log("clicked me");
-    await this._transmit.executeGetRequestPromise(`${RequestMapperService.API_CALL_CHECK}`)
-    .then((res: any) => {
-      console.log(res);
-    })
+    console.log('clicked me');
+    await this._transmit
+      .executeGetRequestPromise(`${RequestMapperService.API_CALL_CHECK}`)
+      .then((res: any) => {
+        console.log(res);
+      });
   }
 }
