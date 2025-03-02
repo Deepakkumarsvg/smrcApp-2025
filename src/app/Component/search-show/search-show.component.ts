eb3584083209701f571779758d59f1313daff97b
Deepak kumar <deepakkumarsvg279@gmail.com>
2025-03-02 16:55:12 +0530

    search results show fix

--- src/app/Component/search-show/search-show.component.ts
+++ src/app/Component/search-show/search-show.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RequestMapperService } from '../../services/request-mapper.service';
+import { TransmissionService } from '../../transmission/transmission.service';
 
 @Component({
   selector: 'app-search-show',
@@ -9,16 +12,41 @@ import { Router } from '@angular/router';
 })
 export class SearchShowComponent implements OnInit {
   public searchResults: any[] = [];
+  private routeSub!: Subscription;
 
-  constructor(private router: Router) {
-    const navigation = this.router.getCurrentNavigation();
-    this.searchResults = navigation?.extras.state?.['searchResults'];
+  constructor(
+    private router: Router,
+    private route: ActivatedRoute,
+    private transmit: TransmissionService
+  ) {
+    // const navigation = this.router.getCurrentNavigation();
+    // this.searchResults = navigation?.extras.state?.['searchResults'];
   }
   ngOnInit(): void {
+    this.routeSub = this.route.queryParams.subscribe((params) => {
+      const searchQuery = params['query'];
+      if (searchQuery) {
+        this.fetchSearchResults(searchQuery);
+      }
+    });
     console.log(this.searchResults); // Use this data in your component
   }
 
+  private async fetchSearchResults(query: string): Promise<void> {
+    console.log(`Fetching data for query: ${query}`);
+    await this.transmit
+      .executeGetRequestPromise(
+        `${RequestMapperService.FETCH_ACTIVITY_LIST}/${query}`
+      )
+      .then((res: any) => {
+        if (res && res.data) {
+          this.searchResults = res.data;
+        }
+      });
+  }
+
   ngOnDestroy(): void {
+    this.routeSub.unsubscribe();
     this.searchResults = [];
   }
 }
