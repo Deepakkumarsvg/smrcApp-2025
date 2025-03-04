a4f68bc976f73b49299d125a7d551e55004c825f
Sushant Kumar <idsushant2793@gmail.com>
2025-03-05 03:55:47 +0530

    env local changed to backend app enginer

--- src/app/environments/environment.local.ts
+++ src/app/environments/environment.local.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
 
-  SERVER_ENDPOINT: 'http://localhost:5000',
+  // SERVER_ENDPOINT: 'http://localhost:5000',
+  SERVER_ENDPOINT: 'https://smrc-backend-2025.el.r.appspot.com',
   // SERVER_ENDPOINT: '',
 };
